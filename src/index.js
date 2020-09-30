ab2a53458c28a6465f9cc7a134adafd68d44bd8b
w1032593545 <1032593545@qq.com>
2020-09-30 11:17:02 +0800

    '初始化框架'

--- src/index.js
+++ src/index.js
@@ -10,3 +10,43 @@ import {
   } from "react-router-dom";
 
 import "./css/style.css"
+import Indexx from './IronMan/index'
+
+
+class Index extends React.Component{
+  render(){
+      return (
+          <Router>
+              <div className='tab-bar'>
+                  <nav>
+                  <ul>
+                      <li>
+                        <Link to="/">首页</Link>
+                      </li>
+                      <li>
+                        <Link to="/category">分类</Link>
+                      </li>
+                  </ul>
+                  </nav>
+              </div>
+              <Switch>
+                  {/* path和Link to 对应写路由 */}
+                  <Route path="/category">
+                      {/* 这里写导入的组件 */}
+                      <Indexx />
+                  </Route>
+                  {/* 首页必须在最后一个 */}
+                  <Route path="/">
+                      <Indexx />
+                  </Route>
+              </Switch>
+          </Router>
+      )
+  }
+}
+
+
+ReactDOM.render(
+  <Index></Index>,
+  document.getElementById('root')
+)
